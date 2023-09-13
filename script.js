4f6d535c7fe4d6b1618606f16a61ec6319f35a59
Alexey Mikhaylov <krummi1414@yandex.ru>
2023-09-13 22:44:33 +0300

    feat: add functionality change status for service btns

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const overlay = document.querySelector('.overlay');
 const menuLink = document.querySelectorAll('.menu__link');
 
 burger.addEventListener('click', e => {
+  e.preventDefault();
   menuList.classList.toggle('menu-active');
   body.classList.toggle('body-menu__active');
   overlay.style.height = document.documentElement.scrollHeight + 'px';
@@ -22,17 +23,18 @@ burger.addEventListener('click', e => {
 
 overlay.addEventListener('click', e => {
   if (e.target.classList.contains('overlay')) {
-    showBurger();
+    e.preventDefault();
+    hideBurger();
   }
 });
 
 menuLink.forEach(item => {
   item.addEventListener('click', e => {
-    showBurger();
+    hideBurger();
   });
 });
 
-function showBurger() {
+function hideBurger() {
   menuList.classList.remove('menu-active');
   body.classList.remove('body-menu__active');
   overlay.classList.remove('overlay__active');
@@ -42,3 +44,62 @@ function showBurger() {
 }
 
 //--------------------------------------------------------------------------------------
+
+
+
+
+//Service filter
+//--------------------------------------------------------------------------------------
+
+const servicesBtn = document.querySelectorAll('.service-filter__btn');
+
+let statusServiceBtn = [false, false, false];
+let counter = 0;
+
+servicesBtn.forEach((item, index) => {
+  item.addEventListener('click', e => {
+    changeStatusBtn(e, item, index);
+    resetStatusBtn(item, index);
+  });
+});
+
+function checkServiceBtn(e, index) {
+  // if (e.target.classList.contains('active-btn')) {
+  //   statusServiceBtn[index] = true;
+  //   console.log(statusServiceBtn);
+  // } else {
+  //   statusServiceBtn[index] = false;
+  //   console.log(statusServiceBtn);
+  // }
+}
+
+function resetStatusBtn(item, index) {
+
+  let value = statusServiceBtn.filter(elem => elem);
+
+  if (value.length > 2) {
+    servicesBtn.forEach((btn, i) => {
+      btn.classList.remove('active-btn');
+
+      if (i !== index) {
+        statusServiceBtn[i] = false;
+      }
+      console.log(statusServiceBtn);
+    });
+
+    item.classList.add('active-btn');
+  }
+}
+
+function changeStatusBtn(e, item, index) {
+
+  if (e.target.classList.contains('active-btn')) {
+    item.classList.remove('active-btn');
+    statusServiceBtn[index] = false;
+    console.log(statusServiceBtn);
+  } else {
+    item.classList.add('active-btn');
+    statusServiceBtn[index] = true;
+    console.log(statusServiceBtn);
+  }
+}
